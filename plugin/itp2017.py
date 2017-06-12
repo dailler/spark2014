155ae0188fd7a322c866b001463a6a61a22dbbed
Sylvain Dailler <dailler@adacore.com>
2017-06-12 17:25:40 +0200

    Communication now working.
    Change-Id: Ibf6ef5195b235ecbe039e99942639bd5e25188a6

--- plugin/itp2017.py
+++ plugin/itp2017.py
@@ -30,12 +30,18 @@ examine_root_project = 'ITP'
 
 class Tree:
 
+    # We have a dictionnary from node_id to row_references because we want an
+    # "efficient" way to get/remove/etc a particular row and we are not going
+    # to go through the whole tree each time (TODO find something that do
+    # exactly this in Gtk).
+    node_id_to_row_ref = {}
+
     def __init__(self):
         # Create a tree that can be appended anywhere
         self.box = Gtk.VBox()
         scroll = Gtk.ScrolledWindow()
         # TODO decide which data we want in this tree
-        self.model = Gtk.ListStore(str, str)
+        self.model = Gtk.TreeStore(str, str, str, str)
         # Create the view as a function of the model
         self.view = Gtk.TreeView(self.model)
         self.view.set_headers_visible(False)
@@ -51,33 +57,64 @@ class Tree:
         cell = Gtk.CellRendererText(xalign=0)
         self.close_col = Gtk.TreeViewColumn("ID")
         self.close_col.pack_start(cell, True)
-        self.close_col.add_attribute(cell, "text", 1)
+        self.close_col.add_attribute(cell, "text", 0)
         self.view.append_column(self.close_col)
+
+        # TODO ???
+        cell = Gtk.CellRendererText(xalign=0)
+        col2 = Gtk.TreeViewColumn("parent")
+        col2.pack_start(cell, True)
+        col2.add_attribute(cell, "text", 2)
+        col2.set_expand(True)
+        self.view.append_column(col2)
 
         # TODO ???
         cell = Gtk.CellRendererText(xalign=0)
         col = Gtk.TreeViewColumn("Name")
         col.pack_start(cell, True)
-        col.add_attribute(cell, "text", 0)
+        col.add_attribute(cell, "text", 1)
         col.set_expand(True)
         self.view.append_column(col)
 
-        # TODO add random node for fun
-        self.add_iter("a", "c")
-        self.add_iter("jfk", "kjofdk")
+        # TODO reinitialize the map from node_id to row_ref
+        self.node_id_to_row_ref = {}
+
+    def add_iter(self, node, parent, name, node_type):
+        if parent == 0:
+            parent_iter = self.model.get_iter_first()
+        else:
+            try:
+                parent_row = self.node_id_to_row_ref[parent]
+                parent_path = parent_row.get_path()
+                parent_iter = self.model.get_iter(parent_path)
+            except:
+                print ("TODO BAD remove this")
+                parent_iter = self.model.get_iter_first()
 
 
-    def add_iter(self, x, y):
-        iter = self.model.append()
-        self.model[iter] = [x, y]
+        iter = self.model.append(parent_iter)
+        self.model[iter] = [str(node), str(parent), name, node_type]
+        path = self.model.get_path(iter)
 
-    def check_notifications(self, p, delimiter, match):
+        row = Gtk.TreeRowReference.new(self.model, path)
+        self.node_id_to_row_ref[node] = row
+        if self.model.iter_has_child(iter):
+            print (True)
+        else:
+            print (False)
+        self.view.expand_all()
+
+    # TODO this is debug
+    def print_notifications(self, notification):
+        a = GPS.Console("Notifications")
+        a.write(notification)
+        GPS.Console()
+
+    def check_notifications(self, unused, delimiter, notification):
         global nb_notif
-        notification = match
+        self.print_notifications(notification)
         nb_notif = nb_notif + 1
         try:
-            if nb_notif == 1:
-                print notification
             p = json.loads(notification)
             parse_notif(p, self)
         except (ValueError):
@@ -104,7 +141,7 @@ def parse_notif(j, tree):
         node_type = j["node_type"] # TODO further change this
         name = j["name"]
         detached = j["detached"]
-        tree.add_iter(str(node_id), str(parent_id))
+        tree.add_iter(node_id, parent_id, name, node_type)
         print "New_node"
     elif notif_type == "Node_change":
         print "Node_change"
@@ -127,35 +164,36 @@ def parse_notif(j, tree):
     else:
         print("Else")
 
-
 def todo (tree):
 
     # Temporary arguments to be given to the gnat_server TODO
     gnat_server = "/home/sdailler/Projet/spark_env/spark2014/install/libexec/spark/bin/gnat_server "
     options = "--why3-conf " + "/home/sdailler/Projet/spark_env/spark2014/why3.conf " + "--standalone " + "--debug "
     file = "/home/sdailler/Projet/spark_env/spark2014/testsuite/gnatprove/temp/tmp-test-itp__example-26211/src/gnatprove/test.mlw"
+    command = ["/home/sdailler/Projet/spark_env/spark2014/install/libexec/spark/bin/gnat_server", "--why3-conf", "/home/sdailler/Projet/spark_env/spark2014/why3.conf", "--standalone", "--debug", file]
 
     # TODO >>>> is temporary
+    # TODO_test = "/home/sdailler/test/test_ocaml/test.byte"
     p = GPS.Process(gnat_server + options + file, regexp=">>>>", on_match=tree.check_notifications)
 
     # TODO generate real requests
-    def send_request(p, timeout):
+    def send_request(p, timeout, node_id, command):
         global n
-        p.send("request" + str(n))
+        # TODO
+        # tree_selection = tree.view.get_selection()
+        # tree_selection.selected_foreach (fun (tree_model, tree_path, tree_iter) -> node_id = tree_model [tree_iter][0])
+        request = "{\"ide_request\": \"Command_req\", \"node_ID\":" + str(node_id) + ", \"command\" : \"" + command + "\" }"
+        print (request)
+        p.send(request)
         n = n + 1
-        print("TODO" + str (n))
+        print("TODO" + str(n))
 
     # Send request automatically... TODO not necessary in the long term
-    t = GPS.Timeout(30000, lambda t: send_request(p, t))
+    t = GPS.Timeout(15000, lambda t: send_request(p, t, 101, "case true"))
     # TODO test
 
 def launch_console(tree):
-    #input_itp = GPS.Console("ITP_input")
-    #input_itp.write("ITP_input")  # Explicit redirection
-
     todo(tree)
-    #p = GPS.Process("why3ide")
-    #p.id = "ITP console"
 
 # TODO never put extra_args because they cannot be removed
 # TODO remove this function which comes from SPARK plugin
@@ -165,7 +203,6 @@ def generic_on_analyze(target, args=[]):
     GPS.Locations.remove_category("Builder results")
     #GPS.execute_action(action="Split horizontally")
     tree = Tree()
-    tree.add_iter("jkhsqd","dhsfk")
     launch_console(tree)
 
 def on_examine_root_project(self):
