f4638acea622f68d25343c5e3f9fe616dc955d5c
Sylvain Dailler <dailler@adacore.com>
2017-07-20 16:52:58 +0200

    Some cleaning of the plugin
    Change-Id: I912b33c275bb4461a0453cad73cc4918155ba79f

--- plugin/itp2017.py
+++ plugin/itp2017.py
@@ -27,12 +27,17 @@ debug_mode = False
 
 examine_root_project = 'ITP'
 
-#TODO add a perspective
+
+def print_debug(s):
+    if debug_mode:
+        print(s)
+
 
 def print_error(message):
     console = GPS.Console("ITP_interactive")
     console.write(message, mode="error")
     console.write("\n> ")
+
 
 def print_message(message):
     console = GPS.Console("ITP_interactive")
@@ -42,7 +47,9 @@ def print_message(message):
 green = Gdk.RGBA(0, 1, 0, 0.2)
 red = Gdk.RGBA(1, 0, 0, 0.2)
 
-# This function converts a string "Not proved" etc into a color for the background of the goal tree
+
+# This function converts a string "Not proved" etc into a color for the
+# background of the goal tree.
 def create_color(s):
     if s == "Proved":
         return green
@@ -61,30 +68,30 @@ def create_color(s):
     else:
         return red
 
+
 # This functions takes a Json object and a proof tree and treat it as a
-# notification on the prooft tree
+# notification on the proof tree. It makes the appropriate update to the tree
+# model.
 # TODO add exceptions
 def parse_notif(j, tree, proof_task):
-    print j
+    print_debug(j)
     # TODO rewrite this
     abs_tree = tree
     tree = tree.tree
     try:
         notif_type = j["notification"]
     except:
-        if debug_mode:
-            print("This is a valid json string but an invalid notification")
-            #TODO
+        print_debug("This is a valid json string but an invalid notification")
     if notif_type == "New_node":
         node_id = j["node_ID"]
         parent_id = j["parent_ID"]
-        node_type = j["node_type"]  # TODO further change this
+        node_type = j["node_type"]
         name = j["name"]
         detached = j["detached"]
         tree.add_iter(node_id, parent_id, name, node_type, "Invalid")
         if abs_tree.first_node <= 0:
             abs_tree.first_node = node_id
-        print "New_node"
+        print_debug("New_node")
     elif notif_type == "Node_change":
         node_id = j["node_ID"]
         update = j["update"]
@@ -116,13 +123,12 @@ def parse_notif(j, tree, proof_task):
                 else:  # In this case it is necessary just a string
                     tree.update_iter(node_id, 4, "proof_attempt")
         else:
-            # TODO
-            print "TODO"
-        print "Node_change"
+            print_debug("TODO")
+        print_debug("Node_change")
     elif notif_type == "Remove":
         node_id = j["node_ID"]
         tree.remove_iter(node_id)
-        print "Remove"
+        print_debug("Remove")
     elif notif_type == "Next_Unproven_Node_Id":
         from_node = j["node_ID1"]
         to_node = j["node_ID2"]
@@ -144,15 +150,15 @@ def parse_notif(j, tree, proof_task):
         proof_task.save(interactive=False)
         proof_task.set_read_only(read_only=True)
         GPS.Console()
-        print notif_type
+        print_debug(notif_type)
     elif notif_type == "File_contents":
-        print notif_type
+        print_debug(notif_type)
     else:
-        print("Else")  # TODO
+        print_debug("TODO Else")
 
     # TODO next_unproven_node_ID is called too many times... Find a way to solve this
     if not (notif_type == "Next_Unproven_Node_Id" or notif_type == "Task" or abs_tree.save_and_exit):
-        get_next_id(abs_tree)
+        abs_tree.get_next_id()
 
 
 def parse_message(j):
@@ -176,7 +182,7 @@ def parse_message(j):
     elif message_type == "Information":
         print_message(message["information"])
     elif message_type == "Task_Monitor":
-        print notif_type  # TODO
+        print_debug(notif_type)  # TODO This can be displayed in GPS
     elif message_type == "Parse_Or_Type_Error":
         print_error(message["error"])
     elif message_type == "Error":
@@ -186,11 +192,11 @@ def parse_message(j):
     elif message_type == "File_Saved":
         print_message(message["information"])
     else:
-        print("Else")  # TODO
+        print_debug("TODO")
 
 
+#  Automatically jumps from from_node to to_node if from_node is selected
 def node_jump_select(tree, from_node, to_node):
-    # TODO this should be a function
     tree_selection = tree.view.get_selection()
     from_node_row = tree.node_id_to_row_ref[from_node]
     from_node_path = from_node_row.get_path()
@@ -211,10 +217,11 @@ def command_request(command, node_id):
         return "{\"ide_request\": \"Save_req\" " + " }"
     elif command == "Remove":
         return ("{\"ide_request\": \"Remove_subtree\", \"node_ID\":" +
-        str(node_id) + " }")
+                str(node_id) + " }")
     else:
         return ("{\"ide_request\": \"Command_req\", \"node_ID\":" +
-        str(node_id) + ", \"command\" : " + json.dumps(command) + " }")
+                str(node_id) + ", \"command\" : " + json.dumps(command) + " }")
+
 
 class Tree:
 
@@ -233,7 +240,7 @@ class Tree:
         self.box.pack_start(scroll, True, True, 0)
 
         # TODO by default append this box to the GPS.MDI
-        GPS.MDI.add(self.box, "Proof Tree", "Proof Tree", group=101, position=4) # TODO find the correct group
+        GPS.MDI.add(self.box, "Proof Tree", "Proof Tree", group=101, position=4) # TODO find the correct groups
 
         cell = Gtk.CellRendererText(xalign=0)
         col2 = Gtk.TreeViewColumn("Name")
@@ -273,16 +280,11 @@ class Tree:
         self.view.append_column(col)
 
         # TODO reinitialize the map from node_id to row_ref
-        # We have a dictionnary from node_id to row_references because we want an
-        # "efficient" way to get/remove/etc a particular row and we are not going
-        # to go through the whole tree each time: O(n) vs O (ln n)
+        # We have a dictionnary from node_id to row_references because we want
+        # an "efficient" way to get/remove/etc a particular row and we are not
+        # going to go through the whole tree each time: O(n) vs O (ln n)
         # TODO find something that do exactly this in Gtk).
-        # TODO unused node_id_to_row_ref = {}
         self.node_id_to_row_ref = {}
-
-        # Make the tree in an independant window of gps
-        # TODO not needed anymore. Should not use it in python and prefer group
-        # GPS.execute_action(action="Split horizontally")
 
     def exit(self):
         self.box.destroy()
@@ -315,7 +317,7 @@ class Tree:
 
         # Append as a child of parent_iter
         new_iter = self.model.append(parent_iter)
-        color = create_color (proved)
+        color = create_color(proved)
         self.model[new_iter] = [str(node), str(parent), name, node_type, proved, color]
         self.set_iter(new_iter, node)
         # TODO expand all at the end ???
@@ -326,7 +328,7 @@ class Tree:
         path = row.get_path()
         iter = self.model.get_iter(path)
         if field == 4:
-            color = create_color (value)
+            color = create_color(value)
             self.model[iter][5] = color
         self.model[iter][field] = value
 
@@ -338,32 +340,13 @@ class Tree:
         self.node_id_to_row_ref.remove(node_id)
 
 n = 0
-nb_notif = 0
-
-# TODO this is also a send_request
-def get_task(tree, node_id):
-    global n
-    request = "{\"ide_request\": \"Get_task\", \"node_ID\":" + str(node_id) + ", \"do_intros\": false}"
-    tree.send(request)
-    n= n + 1
-    print("TODO" + str(n))
-
-def get_next_id(tree):
-    selection = tree.tree.view.get_selection()
-    model, paths = selection.get_selected_rows ()
-    print (paths)
-    if len(paths) == 1:
-        for i in paths:
-            cur_iter = model.get_iter(i)
-            it = model[cur_iter][0]
-            tree.send("{\"ide_request\": \"Get_first_unproven_node\", \"node_ID\":" + it + "}")
 
 
 class Tree_with_process:
     def __init__(self):
         # init local variables
         self.save_and_exit = False
-        print ("ITP launched")
+        print_debug("ITP launched")
 
     def start(self, command):
         # init local variables
@@ -386,7 +369,7 @@ class Tree_with_process:
         tree_selection.set_select_function(self.select_function)
 
         # Define a proof task
-        proof_task_file = GPS.File ("Proof Task", local=True)
+        proof_task_file = GPS.File("Proof Task", local=True)
         self.proof_task = GPS.EditorBuffer.get(proof_task_file, force=True, open=True)
         self.proof_task.set_read_only()
         # TODO should prefer using group and position
@@ -421,10 +404,7 @@ class Tree_with_process:
             self.kill()
 
     def check_notifications(self, unused, delimiter, notification):
-        global nb_notif
-        if debug_mode:
-            print(notification)
-        nb_notif = nb_notif + 1
+        print_debug(notification)
         try:
             # Remove remaining stderr output (stderr and stdout are mixed) by
             # looking for the beginning of the notification (begins with {).
@@ -438,45 +418,57 @@ class Tree_with_process:
         except (TypeError):
             print ("Bad type")
 
-    def select_function (self, select, model, path, currently_selected):
+    def select_function(self, select, model, path, currently_selected):
         tree_iter = model.get_iter(path)
-        #TODO get task instead
-        get_task(self, model[tree_iter][0])
+        self.get_task(model[tree_iter][0])
         return True
 
     def interactive_console_input(self, console, command):
         # TODO
         tree_selection = self.tree.view.get_selection()
         node_id = 0
-        tree_selection.selected_foreach (lambda tree_model, tree_path, tree_iter:
-            self.send_request(tree_model[tree_iter][0], command))
-        print (node_id)
+        tree_selection.selected_foreach(lambda tree_model, tree_path, tree_iter: self.send_request(tree_model[tree_iter][0], command))
+        print_debug(node_id)
 
-    # This is used as a wrapper that actually send the message
-    def send (self, s):
+    # This is used as a wrapper (for debug) that actually sends the message
+    def send(self, s):
         if debug_mode:
-            print_message (s)
+            print_message(s)
         self.process.send(s)
 
     def send_request(self, node_id, command):
-        global n
-        request = command_request (command, node_id)
-        print (request)
+        request = command_request(command, node_id)
+        print_debug(request)
         self.send(request)
-        n = n + 1
-        print("TODO" + str(n))
+
+    # TODO this is also a send_request
+    def get_task(self, node_id):
+        request = "{\"ide_request\": \"Get_task\", \"node_ID\":" + str(node_id) + ", \"do_intros\": false}"
+        self.send(request)
+
+    def get_next_id(self):
+        selection = tree.tree.view.get_selection()
+        model, paths = selection.get_selected_rows()
+        print_debug(paths)
+        if len(paths) == 1:
+            for i in paths:
+                cur_iter = model.get_iter(i)
+                it = model[cur_iter][0]
+                tree.send("{\"ide_request\": \"Get_first_unproven_node\", \"node_ID\":" + it + "}")
+
 
 # TODO put this somewhere where it makes sense
 tree = Tree_with_process()
+
 
 # TODO never put extra_args because they cannot be removed
 # TODO remove this function which comes from SPARK plugin
 def start_ITP(context, args=[]):
     global tree
-    #TODO remove
-    print "[ITP] Launched"
+    print_debug("[ITP] Launched")
+    # TODO what is this ????
     GPS.Locations.remove_category("Builder results")
-    #GPS.execute_action(action="Split horizontally")
+    # GPS.execute_action(action="Split horizontally")
 
     # TODO all these options are already in spark2014.py
     gnat_server = os_utils.locate_exec_on_path("gnat_server")
@@ -491,7 +483,7 @@ def start_ITP(context, args=[]):
             if fnmatch.fnmatch(file, '*.mlw') and mlw_file == "":
                 mlw_file = os.path.join (dir_name, file)
     if mlw_file == "":
-        print "TODO error"
+        print_debug("TODO")
 
     # TODO this context stuff should be done in SPARK and better done...
     print (context)
